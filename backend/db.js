87e7f94ab0a73c94600b9bbccc2b4d97fb83dc66
Gaurav Singh <gauravkalakoti19@gmail.com>
2024-05-17 05:54:34 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 // MongoDB Connection String
 const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI); //mongoURI should be like this--- "mongodb://localhost:27017/table"
 
 // Schema for Users
 const userSchema = new mongoose.Schema({
